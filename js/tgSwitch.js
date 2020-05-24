e613d204b39f3e3660258959e568cb86d7a327a2
Mihail <mishutkavgostyah@gmail.com>
2020-05-24 18:27:08 +0300

    add

--- js/tgSwitch.js
+++ js/tgSwitch.js
@@ -1,1 +1,26 @@
-$("document")
+$('document').ready(function(){
+	$('.tgSwitch').each(function(){
+		if ($(this).find('input').attr('checked')) {
+			$(this).addClass('active');
+			$(this).find('.slider').css({"-webkit-transform" : "translateX(39px)"});
+		}
+	});
+});
+
+let theme = document.querySelector('.theme');
+
+$('.tgSwitch').click(function(){
+	if ($(this).find('input').attr('checked')) {
+		$(this).removeClass('active');
+		$(this).find('input').attr('checked', false);
+		$(this).find('.slider').css({"-webkit-transform" : "translateX(0px)"});
+		theme.textContent = 'светлая тема';
+		document.querySelector('.themeStyle').href="<css/light.css>";
+	} else {
+		$(this).addClass('active');
+		$(this).find('input').attr('checked', true);
+		$(this).find('.slider').css({"-webkit-transform" : "translateX(39px)"});
+		theme.textContent = 'тёмная тема';
+		document.querySelector('.themeStyle').href="<css/dark.css>";
+	}
+});
